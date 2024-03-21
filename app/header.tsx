7804228a191b465e6529d643e7b70d05cd4a52b0
AnderrAlda <anderaldabaldetreku@gmail.com>
2024-03-21 12:08:15 +0100

    improve header and login/logout with the user Avatar

--- app/header.tsx
+++ app/header.tsx
@@ -4,19 +4,68 @@ import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { signIn, signOut, useSession } from "next-auth/react";
 import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogInIcon, LogOutIcon } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+const AccountDropdown = () => {
+  const session = useSession();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        {session.data ? (
+          <Button variant={"outline"}>
+            <Avatar>
+              <AvatarImage src={session.data?.user?.image ?? ""} />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            {session.data?.user?.name}
+          </Button>
+        ) : (
+          <Button onClick={() => signIn("google")}> Sign in</Button>
+        )}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {session.data ? (
+          <DropdownMenuLabel onClick={() => signOut()}>
+            <div className="flex gap-3">
+              <LogOutIcon />
+              <p>Sign Out</p>
+            </div>
+          </DropdownMenuLabel>
+        ) : (
+          <DropdownMenuLabel onClick={() => signIn("google")}>
+            <div className="flex gap-3">
+              <LogInIcon />
+              <p>Sign in</p>
+            </div>
+          </DropdownMenuLabel>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 const Header = () => {
   const session = useSession(); //to now if a user is loged
 
   return (
-    <div>
-      {session.data ? (
-        <Button onClick={() => signOut()}>Sign Out</Button>
-      ) : (
-        <Button onClick={() => signIn("google")}> Sign in</Button>
-      )}
-      <ModeToggle />
-    </div>
+    <header className="bg-gray-50 py-4 dark:bg-gray-900 container mx-auto">
+      <div className="flex justify-between items-center">
+        <div>LOGO</div>
+        <div className="flex items-center gap-5">
+          <AccountDropdown />
+          <ModeToggle />
+        </div>
+      </div>
+    </header>
   );
 };
 
